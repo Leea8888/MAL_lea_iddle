8e6da3e9ee1f1997ecad624e651b73fbdd8c1d78
Leea8888 <l.malet@rns.ecolescreatives.com>
2025-12-04 18:00:44 +0100

    MAJ_2025_12_04
    Coroutine

--- Assets/Scripts/GoldManager.cs
+++ Assets/Scripts/GoldManager.cs
@@ -1,40 +1,81 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using JetBrains.Annotations;
+using System.Collections;
 
 public class GoldManager : MonoBehaviour
 {
-    public int goldAmount;
-    public int power;
-    public TextMeshProUGUI goldText;
-    public TextMeshProUGUI powerText;
+    public int tomatoAmount;
+    public int sauceAmount;
+    public int pastryAmount;
+    public int rollingPinAmount;
+    public TextMeshProUGUI tomatoText;
+    public TextMeshProUGUI sauceText;
+    public TextMeshProUGUI pastryText;
+    public TextMeshProUGUI rollingPinText;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        power = 1;            
-    }
-
-    public void ChangeGold()
-    {
-        goldAmount += power;
-        goldText.text = goldAmount.ToString("00");
-    }
-
-    public void ChangePower()
-    {
-        if (goldAmount > 10)
-        {
-            goldAmount -= 10;
-            goldText.text = goldAmount.ToString("00");
-
-            power += 1;
-            powerText.text = power.ToString("00");
-        }
-    }
-    // Update is called once per frame
-    void Update()
-    {
         
     }
+
+    public void ChangeTomato()
+    {
+        tomatoAmount += 1;
+        tomatoText.text = tomatoAmount.ToString("00");
+    }
+
+    public void ChangeSauce()
+    {
+        if (tomatoAmount > 2)
+        {
+            tomatoAmount -= 1;
+            tomatoText.text = tomatoAmount.ToString("00");
+
+            sauceAmount += 1;
+            sauceText.text = sauceAmount.ToString("00");
+        }
+    }
+
+    public void ChangePastry()
+    { 
+        if (sauceAmount > 2)
+        {
+            sauceAmount -= 1;
+            sauceText.text = sauceAmount.ToString("00");
+
+            pastryAmount += 1;
+            pastryText.text = pastryAmount.ToString("00");
+        }
+    }
+
+    public void ChangeRollingPin()
+    {
+        if (pastryAmount > 2)
+        {
+            pastryAmount -= 1;
+            pastryText.text = pastryAmount.ToString("00");
+
+            rollingPinAmount += 1;
+            rollingPinText.text = rollingPinAmount.ToString("00");
+            StartCoroutine(provokeAutoTomato());
+        }
+    }
+     // Update is called once per frame
+        void Update()
+        {
+
+        }
+        
+        public IEnumerator provokeAutoTomato()
+        {
+            while (true)
+            {
+            yield return new WaitForSeconds(2);
+            ChangeTomato();
+            }
+
+        }
 }
